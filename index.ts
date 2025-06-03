c540ce1882d963bff3ec51118c099802ab70ca00
Tomas Hubelbauer <tomas@hubelbauer.net>
2025-06-03 23:10:45 +0200

    Add endpoints for appending to a file with a password

--- index.ts
+++ index.ts
@@ -1,18 +1,38 @@
 import Bun from "bun";
+import fs from "node:fs";
 
-const FILE_PATH = "/data/data.txt";
+const REPLACE_FILE_PATH = "/data/data.txt";
+const APPEND_FILE_PATH = "/data/data_append.txt";
 
 Bun.serve({
   routes: {
     "/api": {
-      GET: async () => new Response(await Bun.file(FILE_PATH).text()),
+      GET: async () => new Response(await Bun.file(REPLACE_FILE_PATH).text()),
       POST: async (request: Request) => {
-        await Bun.file(FILE_PATH).write(new Date().toISOString());
+        await Bun.file(REPLACE_FILE_PATH).write(new Date().toISOString());
         return new Response();
       },
     },
+    "/:password": {
+      GET: async (request) => {
+        if (request.params.password !== process.env.PASSWORD) {
+          return new Response(null, { status: 401 });
+        }
+
+        return new Response(await Bun.file(APPEND_FILE_PATH).text());
+      },
+      POST: async (request) => {
+        if (request.params.password !== process.env.PASSWORD) {
+          return new Response(null, { status: 401 });
+        }
+
+        const data = await request.text();
+        await fs.promises.appendFile(APPEND_FILE_PATH, data + "\n");
+        return new Response(await Bun.file(APPEND_FILE_PATH).text());
+      },
+    },
   },
-  fetch(request) {
+  fetch() {
     return new Response("Hello from Bun!");
   },
 });
