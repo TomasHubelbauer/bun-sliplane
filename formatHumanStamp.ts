4f4aac87dafa8bbca7e54053e38c7fe8463f0d0d
Tomas Hubelbauer <tomas@hubelbauer.net>
2025-06-11 23:27:56 +0200

    Handle signulars properly in formatHumanStamp and add support for seconds

--- formatHumanStamp.ts
+++ formatHumanStamp.ts
@@ -13,19 +13,29 @@ export default function formatHumanStamp(stamp: string) {
   const diff = now.getTime() - date.getTime();
   const seconds = Math.floor(diff / 1000);
 
-  if (seconds < 60) {
+  if (!seconds) {
     return "now";
   }
 
+  if (seconds < 60) {
+    return `${seconds} second${seconds === 1 ? "" : "s"} ago`;
+  }
+
   if (seconds < 3600) {
-    return `${Math.floor(seconds / 60)} minutes ago`;
+    return `${Math.floor(seconds / 60)} minute${
+      Math.floor(seconds / 60) === 1 ? "" : "s"
+    } ago`;
   }
 
   if (seconds < 86400) {
-    return `${Math.floor(seconds / 3600)} hours ago`;
+    return `${Math.floor(seconds / 3600)} hour${
+      Math.floor(seconds / 3600) === 1 ? "" : "s"
+    } ago`;
   }
 
-  return `${Math.floor(seconds / 86400)} days ago`;
+  return `${Math.floor(seconds / 86400)} day${
+    Math.floor(seconds / 86400) === 1 ? "" : "s"
+  } ago`;
 }
 
 if (import.meta.main) {
