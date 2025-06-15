2fbc41b2358c886cebe7cfe92d8571c99b6938bb
Tomas Hubelbauer <tomas@hubelbauer.net>
2025-06-15 12:00:19 +0200

    Add link monitoring logging for debugging

--- compareLinks.ts
+++ compareLinks.ts
@@ -2,11 +2,17 @@ import type { ServerWebSocket } from "bun";
 import compareLink from "./compareLink.ts";
 import db from "./db.ts";
 
+const READY_STATES = ["connecting", "open", "closing", "closed"] as const;
+
 export default async function compareLinks(
   ws: ServerWebSocket<unknown>,
   reportLogs: boolean
 ) {
-  console.log("Comparing links…", reportLogs);
+  const readyState = READY_STATES[ws.readyState] || "unknown";
+  console.log(
+    `Comparing links for ${ws.data} (${ws.remoteAddress}, ${readyState})…`
+  );
+
   const links = db.query("SELECT * FROM links").all() as {
     url: string;
     checkStamp: string;
--- monitorLinks.ts
+++ monitorLinks.ts
@@ -1,7 +1,13 @@
 import type { ServerWebSocket } from "bun";
 import compareLinks from "./compareLinks.ts";
 
+const READY_STATES = ["connecting", "open", "closing", "closed"] as const;
+
 export default function monitorLinks(ws: ServerWebSocket<unknown>) {
-  console.log("Monitoring links…");
+  const readyState = READY_STATES[ws.readyState] || "unknown";
+  console.log(
+    `Monitoring links for ${ws.data} (${ws.remoteAddress}, ${readyState})…`
+  );
+
   setInterval(() => compareLinks(ws, false), 60 * 1000);
 }
