2f4b286666fc12c8982e19ac2582c11cba20db94
Tomas Hubelbauer <tomas@hubelbauer.net>
2025-06-15 17:23:18 +0200

    Handle Git command not working on the CI

--- getTopCommit.ts
+++ getTopCommit.ts
@@ -1,6 +1,11 @@
 import { $ } from "bun";
 
 export default async function getTopCommit() {
-  const line = await $`git log --oneline -1`.text();
-  return line.match(/^(?<hash>\w{7}) (?<text>.+)\n$/)?.groups;
+  const line = await $`git log --oneline -1`.nothrow().text();
+  const match = line.match(/^(?<hash>\w{7}) (?<text>.+)\n$/);
+  if (match?.groups) {
+    return match.groups;
+  }
+
+  console.log("Failed to parse git log output:", JSON.stringify(line));
 }
