a6879e6e095712a6726723d7189e30f04e2ee737
Tomas Hubelbauer <tomas@hubelbauer.net>
2025-06-18 22:11:52 +0200

    Support stamps in the future in `Stamp`

--- formatHumanStamp.ts
+++ formatHumanStamp.ts
@@ -10,30 +10,33 @@ export default function formatHumanStamp(stamp: string) {
     return "";
   }
 
-  const diff = now.getTime() - date.getTime();
+  const diffSigned = now.getTime() - date.getTime();
+  const diff = Math.abs(diffSigned);
   const seconds = Math.floor(diff / 1000);
 
   if (!seconds) {
     return "now";
   }
 
+  const word = diffSigned < 0 ? "away" : "ago";
+
   if (seconds < 60) {
-    return `${seconds} second${seconds === 1 ? "" : "s"} ago`;
+    return `${seconds} second${seconds === 1 ? "" : "s"} ${word}`;
   }
 
   if (seconds < 3600) {
     return `${Math.floor(seconds / 60)} minute${
       Math.floor(seconds / 60) === 1 ? "" : "s"
-    } ago`;
+    } ${word}`;
   }
 
   if (seconds < 86400) {
     return `${Math.floor(seconds / 3600)} hour${
       Math.floor(seconds / 3600) === 1 ? "" : "s"
-    } ago`;
+    } ${word}`;
   }
 
   return `${Math.floor(seconds / 86400)} day${
     Math.floor(seconds / 86400) === 1 ? "" : "s"
-  } ago`;
+  } ${word}`;
 }
